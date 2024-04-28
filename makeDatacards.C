614d63d71d92c78e8f000e980229572dc6c45e0e
lpaizano <lpaizano@cern.ch>
2024-04-28 08:11:50 -0500

    updates

--- makeDatacards.C
+++ makeDatacards.C
@@ -1,7 +1,6 @@
 #include <string>
 #include "configuration.h"
 
-void makeOneDatacard(TString inputname, TString category, TString wpmin, TString wpmax, std::string name_, TString sample, TString era);
 void makeOneDatacardTop(TString inputname, TString category, TString wpmin, TString wpmax,TString name, TString sample, TString era);
 
 void makeDatacards(TString era, TString sample, TString category, TString wpmin, TString wpmax) {
@@ -37,35 +36,20 @@ void makeOneDatacardTop(TString inputname, TString category, TString wpmin, TStr
   std::ofstream out("./"+inputname_+"/fitdir/datacard_"+label0+"_tt1l_"+category+"_"+wpmin+"to"+wpmax+"_"+era+"_"+name+".txt");
   std::streambuf *coutbuf = std::cout.rdbuf();
   std::cout.rdbuf(out.rdbuf());
-  TString p2name;
-  TString p3name;
-  TString othername;
-  if (category == "top") 
-    {
-      p2name = "tp2";
-      p3name = "tp3";
-      othername = "other";
-    }
-  if (category == "w")
-    {
-      p2name = "tp2";
-      p3name = "tp3";
-      othername ="other";
-    }
-  
+
   // pass 
   TFile *f_p = TFile::Open((TString)inputname+"/"+label0+"_tt1l_"+category+"_"+wpmin+"to"+wpmax+"_"+era+"_"+name+"_templates_p.root","READONLY");
   TH1D  *h_obs_p    = (TH1D*)f_p->Get("data_obs"); 
   TH1D  *h_top_p1_p = (TH1D*)f_p->Get("tp1");
-  TH1D  *h_top_p2_p = (TH1D*)f_p->Get(p2name);
-  TH1D  *h_top_p3_p = (TH1D*)f_p->Get(p3name);
+  TH1D  *h_top_p2_p = (TH1D*)f_p->Get("tp2");
+  TH1D  *h_top_p3_p = (TH1D*)f_p->Get("tp3");
   TH1D  *h_other_p  = (TH1D*)f_p->Get("other");
   // fail
   TFile *f_f = TFile::Open((TString)inputname+"/"+label0+"_tt1l_"+category+"_"+wpmin+"to"+wpmax+"_"+era+"_"+name+"_templates_f.root","READONLY");
   TH1D  *h_obs_f    = (TH1D*)f_f->Get("data_obs");   
   TH1D  *h_top_p1_f = (TH1D*)f_f->Get("tp1");
-  TH1D  *h_top_p2_f = (TH1D*)f_f->Get(p2name);
-  TH1D  *h_top_p3_f = (TH1D*)f_f->Get(p3name);
+  TH1D  *h_top_p2_f = (TH1D*)f_f->Get("tp2");
+  TH1D  *h_top_p3_f = (TH1D*)f_f->Get("tp3");
   TH1D  *h_other_f  = (TH1D*)f_f->Get("other");
   
   std::cout << "imax 2  number of channels\n";
@@ -86,7 +70,7 @@ void makeOneDatacardTop(TString inputname, TString category, TString wpmin, TStr
   std::cout << "# then we list the independent sources of uncertainties, and give their effect (syst. error)\n";
   std::cout << "# on each process and bin\n";
   std::cout << "bin             pass pass pass pass     fail fail fail fail  \n";
-  std::cout << "process         " << p3name << " " << p2name << " tp1 other     " << p3name << " " << p2name << " tp1 other  \n";  
+  std::cout << "process         tp3  tp2  tp1  other    tp3  tp2  tp1  other  \n";  
   std::cout << "process         4 3 6 -7      4 3 6 -7   \n";
   std::cout << "rate            -1 -1 -1 -1     -1 -1 -1 -1  \n"; 
   std::cout << "------------\n";
@@ -100,21 +84,17 @@ void makeOneDatacardTop(TString inputname, TString category, TString wpmin, TStr
   std::cout << "tp1_xsec     lnN      - - 1.05 -    - - 1.05 - \n";
   std::cout << "other_xsec   lnU      - - - 2.00    - - - 2.00 \n";
   
-  // std::cout << "\n";
-  // std::cout << "other_sf   lnN      - - - -    - - - 5.00 \n";
-  
-
   std::cout << "\n";
 
   std::cout << "tp3jms       shapeU    1 - - -     1 - - - \n";  
-  std::cout << "vqqjms       shapeU    - 1 - -     - 1 - - \n";
+  std::cout << "tp2jms       shapeU    - 1 - -     - 1 - - \n";
   std::cout << "tp1jms       shapeU    - - 1 -     - - 1 - \n";
-  std::cout << "otherjms       shapeU    - - - 1     - - - 1 \n";
+  std::cout << "otherjms     shapeU    - - - 1     - - - 1 \n";
 
   std::cout << "tp3jmr       shapeU    1 - - -     1 - - - \n";  
-  std::cout << "vqqjmr       shapeU    - 1 - -     - 1 - - \n";
+  std::cout << "tp2jmr       shapeU    - 1 - -     - 1 - - \n";
   std::cout << "tp1jmr       shapeU    - - 1 -     - - 1 - \n";
-  std::cout << "otherjmr       shapeU    - - - 1     - - - 1 \n";
+  std::cout << "otherjmr     shapeU    - - - 1     - - - 1 \n";
   
   std::cout << "pu          shape    1 1 1 1     1 1 1 1 \n";
   std::cout << "jes         shape    1 1 1 1     1 1 1 1 \n";
@@ -124,14 +104,12 @@ void makeOneDatacardTop(TString inputname, TString category, TString wpmin, TStr
   std::cout << "lhescalemur shape    1 1 1 1     1 1 1 1 \n";
   //std::cout << "lhepdf      shape    1 1 1 1     1 1 1 1 \n";
     
-  std::cout << "norm_top    rateParam    pass    " << p3name << "      1   [0.,10.]\n";
-  std::cout << "norm_top    rateParam    fail    " << p3name << "      1   [0.,10.]\n";
-  std::cout << "norm_top    rateParam    pass    " << p2name << "      1   [0.,10.]\n";
-  std::cout << "norm_top    rateParam    fail    " << p2name << "      1   [0.,10.]\n";
+  std::cout << "norm_top    rateParam    pass    tp3      1   [0.,10.]\n";
+  std::cout << "norm_top    rateParam    fail    tp3      1   [0.,10.]\n";
+  std::cout << "norm_top    rateParam    pass    tp2      1   [0.,10.]\n";
+  std::cout << "norm_top    rateParam    fail    tp2      1   [0.,10.]\n";
   std::cout << "norm_top    rateParam    pass    tp1      1   [0.,10.]\n";
   std::cout << "norm_top    rateParam    fail    tp1      1   [0.,10.]\n";
-  //std::cout << "norm_other    rateParam    pass    other      1   [0.,10.]\n";
-  //std::cout << "norm_other    rateParam    fail    other      1   [0.,10.]\n";
   std::cout << "\n";
 
   std::cout << "*  autoMCStats  0\n";
